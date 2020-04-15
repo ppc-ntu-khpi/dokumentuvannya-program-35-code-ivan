a908829f5424a1c9933a676995cfb9e45c0a483a
Ivan <nazarenkoivan@protonmail.com>
2020-04-15 16:00:38 +0300

    Test package added to documentation

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"domain"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"domain"},{"l":"test"}]
